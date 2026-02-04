8233a5321eb66fd29aa3f9b8f9b0530aa01d0c76
albinchristo04 <priyanka0101111@gmail.com>
2026-02-04 13:45:10 +0530

    oop

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:9999',
+        changeOrigin: true,
+        rewrite: (path) => path
+      }
+    }
+  },
   build: {
     outDir: 'dist',
     sourcemap: false
